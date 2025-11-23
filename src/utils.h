d84ac2af51916936438d28bb351b7025f35734da
Md Maruf Mia <marufmia2811@gmail.com>
2025-11-23 23:07:40 +0600

    Add utility functions in utils.h
    Added utility function declarations for file operations and date checks.

--- src/utils.h
+++ src/utils.h
@@ -1,1 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "medicine.h"
+
+/* File & utility functions */
+void loadFromFile();
+void saveToFile();
+void clear_stdin();
+void pauseConsole();
+int isExpiredOrSoon(int d, int m, int y, int *expiredFlag);
+
+#endif
 
