6afbe60034105f187a3a68fd96999f0405796836
Md Maruf Mia <marufmia2811@gmail.com>
2025-11-23 22:47:52 +0600

    Add medicine structure and function prototypes

--- src/medicine.h
+++ src/medicine.h
@@ -1,1 +1,27 @@
+#ifndef MEDICINE_H
+#define MEDICINE_H
+
+#define MAX_MED 1000
+typedef struct {
+    char name[80];
+    char treatment[80];
+    int quantity;
+    float price;
+    int day, month, year;
+    int shelf, row;
+    int id;
+} Medicine;
+
+/* Function prototypes */
+void addMedicine();
+void displayAll();
+int searchMedicineByName(const char *name);
+void updateMedicine();
+void deleteMedicine();
+void checkExpiry();
+void findAlternatives();
+void showShelfLocation();
+void searchByTreatment();
+
+#endif
 
