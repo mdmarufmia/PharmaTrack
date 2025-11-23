d0f57f6b0369d793353cdb2c7b255ab746461d32
Md Maruf Mia <marufmia2811@gmail.com>
2025-11-23 22:47:29 +0600

    Add main function and menu structure

--- src/main.c
+++ src/main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include "medicine.h"
+
+int main() {
+    int choice;
+    loadFromFile(); // from utils.c
+
+    while(1) {
+        printf("Menu...\n");
+        scanf("%d", &choice);
+        // switch-case calling medicine/billing functions
+    }
+}
 
