89d49b99a9bffe115339aaaea2c61cbad4a5ae8c
Md Maruf Mia <marufmia2811@gmail.com>
2025-11-23 22:49:17 +0600

    Add utility functions for console and file handling
    Added utility functions for file operations and date checks.

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+#include "medicine.h"
+
+void clear_stdin() { /* ... */ }
+void pauseConsole() { /* ... */ }
+void loadFromFile() { /* ... */ }
+void saveToFile() { /* ... */ }
+int isExpiredOrSoon(int d,int m,int y,int *expiredFlag) { /* ... */ }
 
